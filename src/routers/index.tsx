cde783f0b651f11c172891d20f6ebe7b9d69681d
Abdulhamid <abdulhamid.fawal@itland-sy.com>
2021-09-13 11:00:21 +0300

    - routers

---
+++ src/routers/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Login } from '../pages/authentication/log-in';
+interface Props {
+    
+}
+
+export const Routers = (props: Props) => {
+    return (
+      <div>
+        <Router>
+          <div>
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route exact path="/">
+                <Login />
+              </Route>
+              <Route path="/Login">
+                <Login />
+              </Route>
+              <Route path="/NotFound">
+               <h1>404</h1>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+}
